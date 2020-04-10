044b5d9dd8a359c370af194446fc180f24e9bbde
omarAbdElAziz Mahmoud El Qady <omaralkadi111@yahoo.com>
2020-04-11 01:57:27 +0200

    circule

---
+++ shapes/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace shapes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.XCenter = new System.Windows.Forms.TextBox();
+            this.YCenter = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.RadiusText = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // XCenter
+            // 
+            this.XCenter.Location = new System.Drawing.Point(529, 23);
+            this.XCenter.Name = "XCenter";
+            this.XCenter.Size = new System.Drawing.Size(100, 20);
+            this.XCenter.TabIndex = 0;
+            // 
+            // YCenter
+            // 
+            this.YCenter.Location = new System.Drawing.Point(529, 60);
+            this.YCenter.Name = "YCenter";
+            this.YCenter.Size = new System.Drawing.Size(100, 20);
+            this.YCenter.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(475, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X Center";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(475, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Y Center";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(475, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Radius";
+            // 
+            // RadiusText
+            // 
+            this.RadiusText.Location = new System.Drawing.Point(529, 94);
+            this.RadiusText.Name = "RadiusText";
+            this.RadiusText.Size = new System.Drawing.Size(100, 20);
+            this.RadiusText.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(529, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 405);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RadiusText);
+            this.Controls.Add(this.YCenter);
+            this.Controls.Add(this.XCenter);
+            this.Name = "Form1";
+            this.Text = "Omar Tarek ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox XCenter;
+        private System.Windows.Forms.TextBox YCenter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox RadiusText;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ shapes/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace shapes
+{
+    public partial class Form1 : Form
+    {
+        Graphics global_g;
+        int CenterX, CenterY, RadiusValue;
+        bool DrawFlag = false;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Width = 680;
+            this.Height = 550;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (this.DrawFlag == true)
+            {
+                Graphics g = e.Graphics;
+                int x = 0;
+                int y = this.RadiusValue;
+                int p = 1 - this.RadiusValue;
+                Bitmap bm = new Bitmap(1, 1);
+                bm.SetPixel(0, 0, Color.Red);
+                while (x < y)
+                {
+                    x++;
+                    if (p < 0)
+                    {
+                        p += 2 * x + 1;
+                    }
+                    else
+                    {
+                        y--;
+                        p += 2 * (x - y) + 1;
+                    }
+                    int xCenter = this.CenterX;
+                    int yCenter = this.CenterY;
+                    g.DrawImageUnscaled(bm, xCenter + x, yCenter + y);
+                    g.DrawImageUnscaled(bm, xCenter - x, yCenter + y);
+                    g.DrawImageUnscaled(bm, xCenter + x, yCenter - y);
+                    g.DrawImageUnscaled(bm, xCenter - x, yCenter - y);
+                    g.DrawImageUnscaled(bm, xCenter + y, yCenter + x);
+                    g.DrawImageUnscaled(bm, xCenter - y, yCenter + x);
+                    g.DrawImageUnscaled(bm, xCenter + y, yCenter - x);
+                    g.DrawImageUnscaled(bm, xCenter - y, yCenter - x);
+                }
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!(XCenter.Text == "" || YCenter.Text == "" || RadiusText.Text ==""))
+            {
+                bool test =  Int32.TryParse(XCenter.Text, out int XCenter1);
+                bool test2 = Int32.TryParse(YCenter.Text, out int YCenter1);
+                bool test3 = Int32.TryParse(RadiusText.Text, out int Radius1);
+                if (test && test2 && test3)
+                {
+                    this.CenterX = XCenter1;
+                    this.CenterY = YCenter1;
+                    this.RadiusValue = Radius1;
+                    this.DrawFlag = true;
+                    Refresh();
+                }
+                else
+                {
+                    //must enter integer number.
+                }
+            }
+        }
+
+
+    }
+}
